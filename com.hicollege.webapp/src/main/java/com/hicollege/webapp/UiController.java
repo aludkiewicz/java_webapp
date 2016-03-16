d3583333c0d6364fd787c77f3dfb947d3914076c
Alexander Ludkiewicz <alexander.ludkiewicz@polystar.com>
2016-03-16 20:36:39 +0100

    Saving work up until now. Added Hibernate configs, started mapping out DTOs into the DB and added a DAO for retrieving users from the database

--- com.hicollege.webapp/src/main/java/com/hicollege/webapp/UiController.java
+++ com.hicollege.webapp/src/main/java/com/hicollege/webapp/UiController.java
@@ -5,16 +5,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.hicollege.webapp.dtos.User;
 
 //@EnableAutoConfiguration
 @Controller
 public class UiController {
+    
+    @Autowired
+    private UserDao dao;
 	
 
     @RequestMapping("/hello")
@@ -25,18 +27,39 @@ public class UiController {
     
     @RequestMapping(value = "/ui", method = RequestMethod.GET)
     public String getUI() throws IOException {
+        User user = new User(1, "Alexander", "27", "alexander.ludkiewicz@hiq.se", new ArrayList<>());
+        dao.saveOrUpdate(user);
         return "ui/ui";
     }
     
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    @RequestMapping(value = "/find/allusers", method = RequestMethod.GET)
     @ResponseBody
     public List<User> getUsers() {
-        return Arrays.asList(new User("Alexander", "27", "alexander.ludkiewicz@hiq.se", new ArrayList<>()));
+        return Arrays.asList(new User(1, "Alexander", "27", "alexander.ludkiewicz@hiq.se", new ArrayList<>()));
     }
     
-    @RequestMapping(value = "/create/user/derp/foo/bar/lol/what", method = RequestMethod.GET)
+    @RequestMapping(value = "/find/user/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public String createUser() {
+    public List<User> getUser(@PathVariable int id) {
+        return Arrays.asList(new User(id, "Alexander", "27", "alexander.ludkiewicz@hiq.se", new ArrayList<>()));
+    }
+    
+    @RequestMapping(value = "/delete/user/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public List<User> deleteUser(@PathVariable int id) {
+        return Arrays.asList(new User(id, "Alexander", "27", "alexander.ludkiewicz@hiq.se", new ArrayList<>()));
+    }
+    
+    @RequestMapping(value = "/create/user/", method = RequestMethod.GET)
+    @ResponseBody
+    public String createUser(
+        @RequestParam(value = "username", required = true)  String username,
+        @RequestParam(value = "age",      required = true)  int age,
+        @RequestParam(value = "email",    required = false) String email,
+        @RequestParam(value = "albums",   required = true)  List<String> albumNames) {
+        
+        //User newUser = new User(id, name, age, email, albums)
+        
         return "Derp";
     }
 
--- com.hicollege.webapp/src/main/java/com/hicollege/webapp/dtos/User.java
+++ com.hicollege.webapp/src/main/java/com/hicollege/webapp/dtos/User.java
@@ -2,48 +2,68 @@ package com.hicollege.webapp.dtos;
 
 import java.util.List;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
     
+    private int id;
     private String name;
     private String age;
     private String email;
     private List<Album> albums;
     
-    public User(String name, String age, String email, List<Album> albums) {
+    public User(int id, String name, String age, String email, List<Album> albums) {
+        this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
         this.albums = albums;
     }
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Column(name = "name")
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
     
+    @Column(name = "age")
     public String getAge() {
         return age;
     }
-    
+
     public void setAge(String age) {
         this.age = age;
     }
     
+    @Column(name = "email")
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public List<Album> getAlbums() {
         return albums;
     }
-    
+
     public void setAlbums(List<Album> albums) {
         this.albums = albums;
     }
@@ -55,6 +75,7 @@ public class User {
         result = prime * result + ((age == null) ? 0 : age.hashCode());
         result = prime * result + ((albums == null) ? 0 : albums.hashCode());
         result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + id;
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
@@ -82,6 +103,8 @@ public class User {
             if (other.email != null)
                 return false;
         } else if (!email.equals(other.email))
+            return false;
+        if (id != other.id)
             return false;
         if (name == null) {
             if (other.name != null)
